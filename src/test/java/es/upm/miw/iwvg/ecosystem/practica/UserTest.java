2252fff06be8bcaa5438f23ee484f1a12e0f8343
Fernanda <guerragil.fernanda@gmail.com>
2019-09-24 23:45:09 +0200

    git commit -m "Added UserTest class"

---
+++ src/test/java/es/upm/miw/iwvg/ecosystem/practica/UserTest.java
@@ -0,0 +1,30 @@
+package es.upm.miw.iwvg.ecosystem.practica;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserTest {
+
+    private User user;
+
+    @BeforeEach
+    void before() { user = new User(1, "Fernanda", "Guerra"); }
+
+    @Test
+    void testUserFullName() {assertEquals("Fernanda Guerra", user.fullName());}
+
+    @Test
+    void testUserInitials() {assertEquals("F.", user.initials());}
+
+    @Test
+    void testUserNumber() {assertEquals(1, user.getNumber());}
+
+    @Test
+    void testUserName() {assertEquals("Fernanda", user.getName());}
+
+    @Test
+    void testUserFamilyName() {assertEquals("Guerra", user.getFamilyName());}
+
+}
