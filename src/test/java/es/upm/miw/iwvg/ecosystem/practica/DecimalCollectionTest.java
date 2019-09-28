a0bc7a24f137a4b1e73b7a621ce68b223e5ee8c8
Fernanda <guerragil.fernanda@gmail.com>
2019-09-28 17:30:05 +0200

    git commit -m "Added DecimalCollectionTest class"

---
+++ src/test/java/es/upm/miw/iwvg/ecosystem/practica/DecimalCollectionTest.java
@@ -0,0 +1,28 @@
+package es.upm.miw.iwvg.ecosystem.practica;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DecimalCollectionTest {
+
+    private DecimalCollection decimalCollection;
+
+    @BeforeEach
+    void before() {
+        this.decimalCollection = new DecimalCollection();
+        this.decimalCollection.add(2.45);
+        this.decimalCollection.add(5.69);
+        this.decimalCollection.add(4.1);
+    }
+
+    @Test
+    void testSize() { assertEquals(3, this.decimalCollection.size()); }
+
+    @Test
+    void testSum() { assertEquals(12.24, this.decimalCollection.sum(), 10e-5); }
+
+    @Test
+    void testHigher() { assertEquals(5.69, this.decimalCollection.higher(), 10e-5); }
+}
