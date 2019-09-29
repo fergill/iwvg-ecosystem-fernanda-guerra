9e252a2f0f38b62314ce45cc3c47de907e58f465
Fernanda <guerragil.fernanda@gmail.com>
2019-09-30 01:41:39 +0200

    Improvements on Point and PointTest classes

--- src/test/java/es/upm/miw/iwvg/ecosystem/practica/PointTest.java
+++ src/test/java/es/upm/miw/iwvg/ecosystem/practica/PointTest.java
@@ -9,19 +9,20 @@ class PointTest {
     private Point point;
 
     @BeforeEach
-    void before() { point = new Point(4,5); }
+    void before() { point = new Point(4,5,6); }
 
     @Test
-    void testModule() { assertEquals(6.4031, point.module(),10e-5); }
+    void testModule() { assertEquals(8.7749, point.module(),10e-5); }
 
     @Test
     void testPhase() { assertEquals(0.8960, point.phase(), 10e-5); }
 
     @Test
     void testTranslateOrigin() {
-        this.point.translateOrigin(new Point(1, 2));
+        this.point.translateOrigin(new Point(1, 2, 3));
         assertEquals(3, point.getX());
         assertEquals(3, point.getY());
+        assertEquals(3, point.getZ());
     }
 
     @Test
@@ -29,5 +30,8 @@ class PointTest {
 
     @Test
     void testPointY() { assertEquals(5, point.getY()); }
+
+    @Test
+    void testPointZ() { assertEquals(6, point.getZ()); }
 
 }
